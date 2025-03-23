7fee4a36882a97d91db6db2685ed2d2bedddbcaf
fitosoumik <68319099+fitosoumik@users.noreply.github.com>
2025-03-23 21:05:07 +0530

    Add unit tests for calculator

---
+++ test_calculator.py
@@ -0,0 +1,23 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(1, 2), 3)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 2), 8)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(8, 4), 2)
+        with self.assertRaises(ValueError):
+            self.calc.divide(1, 0)
+
+if __name__ == '__main__':
+    unittest.main()
